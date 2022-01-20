d42b6b8f0e4a753e8ad227d0534de2c5bbfce584
Vaibhav Sharma <76773190+AlphaVS-76@users.noreply.github.com>
2022-01-20 18:36:24 +0530

    Update BankingSystem.cpp

--- BankingSystem.cpp
+++ BankingSystem.cpp
@@ -10,67 +10,52 @@ private:
     long accNo;
     long balanceAmt;
 public:
-    void display();
-    void newAccount();
-    void deposit();
-    void withdrawl();
+    void display(){
+        cout << "\n\t\t\tBanking System" << endl << "*****************************************************************" << endl << "\t\tAccount holder name: " << name << endl << "\t\tBalance in Account: " << balanceAmt << endl << "*****************************************************************" << endl;
+    }
+    
+    void newAccount(){
+        fflush(stdin);
+        cout << "Enter your name: ";
+        getline(cin, name);
+        cout << "Enter account number: ";
+        cin >> accNo;
+        cout << "Enter balance amount: ";
+        cin >> balanceAmt;
+    }
+    
+    void deposit(){
+        fflush(stdin);
+        cout << "Enter your name: ";
+        getline(cin, name);
+        cout << "Enter amount to be deposited(>= 500): ";
+        cin >> balanceAmt;
+    }
+    
+    void withdrawl() {
+        int wd;
+        fflush(stdin);
+        cout << "Enter your name: ";
+        getline(cin, name);
+        cout << "Enter balance amount in account: ";
+        cin >> balanceAmt;
+        cout << "Enter the amount to be withdrawn: ";
+        cin >> wd;
+        balanceAmt = balanceAmt - wd;
+    
+        if(balanceAmt >= 500)
+            cout << "Your balance amount is: " << balanceAmt << endl;
+        else
+            cout << "Your balance is below minimum!" << endl;
+    }
 };
-
-void Banking::deposit(){
-    fflush(stdin);
-    cout << "Enter your name: ";
-    getline(cin, name);
-    cout << "Enter amount to be deposited(>= 500): ";
-    cin >> balanceAmt;
-}
-
-void Banking::newAccount(){
-    fflush(stdin);
-    cout << "Enter your name: ";
-    getline(cin, name);
-    cout << "Enter account number: ";
-    cin >> accNo;
-    cout << "Enter balance amount: ";
-    cin >> balanceAmt;
-}
-
-void Banking::display(){
-    cout << "\n\t\t\tBanking System" << endl;
-    cout << "*****************************************************************" << endl;
-    cout << "\t\tAccount holder name: " << name << endl;
-    cout << "\t\tBalance in Account: " << balanceAmt << endl;
-    cout << "*****************************************************************" << endl;
-}
-
-void Banking::withdrawl() {
-    int wd;
-    fflush(stdin);
-    cout << "Enter your name: ";
-    getline(cin, name);
-    cout << "Enter balance amount in account: ";
-    cin >> balanceAmt;
-    cout << "Enter the amount to be withdrawn: ";
-    cin >> wd;
-    balanceAmt = balanceAmt - wd;
-    if(balanceAmt >= 500){
-        cout << "Your balance amount is: " << balanceAmt << endl;
-    }
-    else{
-        cout << "Your balance is below minimum!" << endl;
-    }
-}
 
 int main() {
     char choice;
     int num;
     Banking a1;
 
-    cout << "\t1. NewAccount" << endl;
-    cout << "\t2. Deposit" << endl;
-    cout << "\t3. Withdraw" << endl;
-    cout << "\t4. Current Balance" << endl;
-    cout << "\t5. Exit" << endl;
-    cout << "Enter your choice: " << endl;
+    cout << "\t1. NewAccount" << endl << "\t2. Deposit" << endl << "\t3. Withdraw" << endl << "\t4. Current Balance" << endl << "\t5. Exit" << endl << "Enter your choice: " << endl;
     cin >> num;
 
     switch (num) {
